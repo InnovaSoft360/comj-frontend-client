54e8e31795f9f95e720422c437d72d11331ba5d2
Adyllson <domingosady11son@gmail.com>
2025-08-14 16:37:06 +0100

    first commit

---
+++ src/app/config/App.tsx
@@ -0,0 +1,13 @@
+import './../style/App.css'
+import './../style/index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline"> Hello world!  </h1>
+    </>
+  )
+}
+
+export default App
