42156f7e3cb032184a3865dccc83b6926bb81253
Adyllson <domingosadyllsxn@gmail.com>
2025-08-16 15:42:31 +0100

    estiliza o hero-section part 3

---
+++ src/features/public/home/hero-section/index.tsx
@@ -0,0 +1,21 @@
+import heroBg from "../../../../assets/section/1 Image 2025-08-16 at 10.55.38.png";
+import "./style.css";
+export default function HeroSection() {
+  return (
+    <section className="hero-section" style={{ backgroundImage: `url(${heroBg})` }}>
+      <div className="style-Main">
+        <div className="hero-section-content">
+          <div className="hero-section-info">
+            <h2>
+              Condomínio <span>Osvaldo MJ</span>
+            </h2>
+            <p>
+              Residências T3 modernas e seguras para militares e suas famílias,
+              localizadas na Província do Icolo e Bengo, Bairro Zango-4.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
