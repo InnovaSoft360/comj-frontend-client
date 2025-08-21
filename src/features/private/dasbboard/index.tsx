586edcdca11cb6f0c5c9f483823b2099f269931d
Adyllson <domingosadyllsxn@gmail.com>
2025-08-21 12:43:25 +0100

    Cria a parte do adm

--- src/features/private/dasbboard/index.tsx
+++ src/features/private/dasbboard/index.tsx
@@ -1,5 +1,32 @@
-export default function Dashboard () {
+import SideBar from "../../../components/layouts/SideBar";
+import DashboardCards from "./DashboardCards";
+import "./style.css";
+import { useState } from "react";
+
+export default function Dashboard() {
+  const [activePage, setActivePage] = useState("Dashboard");
+
+  const renderPage = () => {
+    switch (activePage) {
+      case "Dashboard":
+        return <DashboardCards />;
+      case "GestaoUsuario":
+        return <h2>Gestão de Usuário</h2>;
+      case "GestaoCandidatura":
+        return <h2>Gestão de Candidatura</h2>;
+      case "GestaoImovel":
+        return <h2>Gestão de Imóvel</h2>;
+      default:
+        return <DashboardCards />;
+    }
+  };
+
   return (
-    <div>Dashboard</div>
-  )
+    <div className="app-container">
+      <SideBar onSelect={setActivePage} />
+      <main className="main-content">
+        {renderPage()}
+      </main>
+    </div>
+  );
 }
